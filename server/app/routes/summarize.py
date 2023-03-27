632ba192987d10049bf454e48e7f2a37b66af642
rshandilya <prof.rshandilya@gmail.com>
2023-03-27 14:47:12 +0530

    summary

--- server/app/routes/summarize.py
+++ server/app/routes/summarize.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Form
 from app.utils.apicalls import judgement_list, judgement_detail
+from app.utils.summarizer import summarizer
+from typing import Annotated
+from app.schemas.summary import JudgementText
 
 router = APIRouter(
     prefix="/summary"
@@ -19,5 +22,15 @@ def get_judgement_list(tid:int):
     if not data:
         raise HTTPException(status_code=404, detail="Item not downloaded")
     return data
+
+@router.post("/judgement/summary", response_model=None)
+def judgement_summary(
+    content: Annotated[str, Form()]
+):
+    result = summarizer(content)
+    if not result:
+        raise HTTPException(status_code=404, detail="Error in Sumarizing")
+    return {"summary": result}
+
 
 
